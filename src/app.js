34545a394501308a92525505cd554078069b6143
YoonSung Kim <opqudk@gmail.com>
2021-06-18 14:14:30 +0900

    resolved: #4

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const ajax = new XMLHttpRequest();
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-
 function getData(url) {
 	ajax.open('GET', url, false);
 	ajax.send();
