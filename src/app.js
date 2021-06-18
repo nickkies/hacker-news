ff70aca47876b5806d19cdff142ec84eba16e137
YoonSung Kim <opqudk@gmail.com>
2021-06-18 11:43:02 +0900

    list & detail

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
+const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
-const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
+const content = document.createElement('div');
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
 ajax.open('GET', NEWS_URL, false);
 ajax.send();
@@ -7,12 +10,31 @@ ajax.send();
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement('ul');
 
+window.addEventListener('hashchange', () => {
+    const id = location.hash.substr(1);
+
+    ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+    ajax.send();
+
+    const newsContent = JSON.parse(ajax.response);
+    const title = document.createElement('h1');
+
+    title.innerHTML = newsContent.title;
+    content.appendChild(title);
+
+});
+
 newsFeed.forEach( news => {
     const li = document.createElement('li');
+    const a = document.createElement('a');
 
-    li.innerHTML = news.title;
+    a.href= `#${news.id}`;
+    a.innerHTML = `${news.title}(${news.comments_count})`;
 
+    li.appendChild(a);
     ul.appendChild(li);
 });
 
-document.getElementById('root').appendChild(ul);
+container
+    .appendChild(ul)
+    .appendChild(content);
