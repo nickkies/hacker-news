fcaa1a365eae1c14ef55dc43667e4532820a4d0d
YoonSung Kim <opqudk@gmail.com>
2021-06-18 14:37:53 +0900

    page nation

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
-const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
-const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+const NEWS_URL = 'http://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'http://api.hnpwa.com/v0/item/@id.json';
+const store = {
+	currentPage: 1,
+};
 
 function getData(url) {
 	ajax.open('GET', url, false);
@@ -16,27 +19,41 @@ function newsFeed() {
 
 	newsList.push('<ul>');
 
-	newsFeed.forEach( news => {
+	for ( let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++ ) {
+		const news = newsFeed[i];
+
 		newsList.push(`
 	    <li>
-	      <a href="#${news.id}">${news.title}(${news.comments_count})</a>
+	      <a href="#/show/${news.id}">
+					${news.title}(${news.comments_count})
+				</a>
 	    </li>
 	  `);
-	});
+	}
 
 	newsList.push('</ul>');
+
+	const cp = store.currentPage,
+		tp = Math.ceil(newsFeed.length);
+
+	newsList.push(`
+		<div>
+			<a href="#/page/${cp> 1 ? cp - 1 : 1}">이전 페이지</a>
+			<a href="#/page/${cp < tp ? cp + 1 : tp}">다음 페이지</a>
+		</div>
+	`);
 
 	container.innerHTML = newsList.join('');
 }
 
 function newsDetail() {
-	const id = location.hash.substr(1);
+	const id = location.hash.substr(7);
 	const newsContent = getData(CONTENT_URL.replace('@id', id));
 
 	container.innerHTML = `
     <h1>${newsContent.title}</h1>
     <div>
-        <a href="#">목록으로</a>
+        <a href="#/page/${store.currentPage}">목록으로</a>
     </div>
   `;
 }
@@ -45,6 +62,9 @@ function router() {
 	const routePath = location.hash;
 
 	if ( routePath === '' ) {
+		newsFeed();
+	} else if ( routePath.indexOf('#/page/') >= 0 ) {
+		store.currentPage = Number(routePath.substr(7));
 		newsFeed();
 	} else {
 		newsDetail();
