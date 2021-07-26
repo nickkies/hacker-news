d360ccb0fd9b08edabea436cf5fcc5c64b431a32
YoonSung Kim <opqudk@gmail.com>
2021-07-26 14:04:53 +0900

    fetch & promise

--- src/core/api.ts
+++ src/core/api.ts
@@ -2,14 +2,23 @@ import { NewsFeed, NewsDetail } from '../types';
 import { NEWS_URL, CONTENT_URL } from '../config';
 
 export class Api {
-	getRequest<AjaxResponse>(url: string, cb: (data: AjaxResponse) => void): void {
-    const ajax = new XMLHttpRequest();
-		ajax.open('GET', url);
-		ajax.addEventListener('load', () => {
-			cb(JSON.parse(ajax.response));
+	getRequestWithXHR<AjaxResponse>(url: string, cb: (data: AjaxResponse) => void): void {
+    const xhr = new XMLHttpRequest();
+		xhr.open('GET', url);
+		xhr.addEventListener('load', () => {
+			cb(JSON.parse(xhr.response));
 		});
 
-		ajax.send();
+		xhr.send();
+	}
+
+	getRequest<AjaxResponse>(url: string, cb: (data: AjaxResponse) => void): void {
+		fetch(url)
+			.then(response => response.json())
+			.then(cb)
+			.catch(() => {
+				console.error('데이터를 불러오지 못했습니다.');
+			});
 	}
 }
 
@@ -27,14 +36,20 @@ function applyApiMixins(targetClass: any, baseClasses: any[]): void {
 }
 
 export class NewsFeedApi {
+	getDataWithXHR(cb: (data: NewsFeed[]) => void): void {
+		return this.getRequestWithXHR<NewsFeed[]>(NEWS_URL, cb);
+	}
 	getData(cb: (data: NewsFeed[]) => void): void {
 		return this.getRequest<NewsFeed[]>(NEWS_URL, cb);
 	}
 }
 
 export class NewsDetailApi {
+	getDataWithXHR(id: string, cb: (data: NewsDetail) => void): void {
+		return this.getRequestWithXHR(CONTENT_URL.replace('@id', id), cb);
+	}
 	getData(id: string, cb: (data: NewsDetail) => void): void {
-		return this.getRequest<NewsDetail>(CONTENT_URL.replace('@id',id), cb)
+		return this.getRequest<NewsDetail>(CONTENT_URL.replace('@id', id), cb);
 	}
 }
 
